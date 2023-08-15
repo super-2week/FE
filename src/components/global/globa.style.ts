1e4887a201c9a3f1b95d694c3a4845d5fb41c3b3
Sim <0doogadooga@gmail.com>
2023-08-16 03:39:25 +0900

    feat #14 : navi별 로고 active 완료, 호버(기본 흰색 로고 미완료)

--- src/components/global/globa.style.ts
+++ src/components/global/globa.style.ts
@@ -1,4 +1,18 @@
-import { styled } from "styled-components";
+import styled, { css } from "styled-components";
+
+import dog from "../../asset/dog.png";
+import cat from "../../asset/cat.png";
+import small from "../../asset/smallAnimal.png";
+import other from "../../asset/other.png";
+
+import bluedog from "../../asset/bluedog.png";
+import bluecat from "../../asset/bluecat.png";
+import bluesmall from "../../asset/bluesmallAnimal.png";
+import blueother from "../../asset/blueother.png";
+
+interface IconProps {
+  categori: string; // 카테고리에 맞는 타입으로 수정
+}
 
 export const HeaderWrap = styled.div`
   width: 100%;
@@ -40,7 +54,7 @@ export const HeaderWrap = styled.div`
 
 export const NaviWrap = styled.ul`
   width: 100%;
-  height: 70px;
+  height: 60px;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -52,15 +66,26 @@ export const NaviWrap = styled.ul`
     display: flex;
     align-items: center;
     justify-content: center;
+    gap: 10px;
     padding: 0 20px;
     border-radius: 20px;
     height: 65%;
     box-sizing: border-box;
     cursor: pointer;
     font-weight: bold;
+    transform: scale(1);
+    transition: transform 0.1s;
     &:hover {
-      border: 1px solid #fff;
+      transform: scale(1.05);
     }
+    /* img {
+      width: 20px;
+      height: 20px;
+    }
+    .icon {
+      width: 20px;
+      height: 20px;
+    } */
   }
   .active {
     background-color: #fff;
@@ -68,4 +93,31 @@ export const NaviWrap = styled.ul`
     border: 1px solid #fff;
     box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.05);
   }
+`;
+
+export const Icon = styled.div<IconProps>`
+  width: 20px;
+  height: 20px;
+  ${({ categori }) => {
+    switch (categori) {
+      case "강아지":
+        return css`
+          background: url(${bluedog}) no-repeat center / cover;
+        `;
+      case "고양이":
+        return css`
+          background: url(${bluecat}) no-repeat center / cover;
+        `;
+      case "소동물":
+        return css`
+          background: url(${bluesmall}) no-repeat center / cover;
+        `;
+      case "기타":
+        return css`
+          background: url(${blueother}) no-repeat center / cover;
+        `;
+      default:
+        return css``;
+    }
+  }}
 `;
