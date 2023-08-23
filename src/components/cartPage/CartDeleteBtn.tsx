38ea307adbd9cfa7a8204b0a53a93b0ef4d25f16
whtnqls124578 <whtnqls124578@naver.com>
2023-08-23 22:39:17 +0900

    feat:#51 cartpage-detele axios 통신 후 상품 삭제완료

--- src/components/cartPage/CartDeleteBtn.tsx
+++ src/components/cartPage/CartDeleteBtn.tsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Deletecart } from '../../apis/cart/deleteCart.api';
 
 type DeleteProps = {
     cartId: number
+    onDeleteCart: (cartId: number) => void;
 }
 
-const CartDeleteBtn = ({ cartId }: DeleteProps) => {
+const CartDeleteBtn = ({ cartId, onDeleteCart }: DeleteProps) => {
+
+    const token = localStorage.getItem("accesstoken");
+    console.log('test1', token)
+
+    const deleteCarts = () => {
+        Deletecart(cartId, token);
+        onDeleteCart(cartId);
+    }
+
     return (
         <div className="product_delete_wrapper">
-            <button>x</button>
+            <button onClick={deleteCarts}>x</button>
         </div>
     )
 }
