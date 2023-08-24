68893b187667adeb4fd4b47667eb88879bbad559
whtnqls124578 <whtnqls124578@naver.com>
2023-08-24 16:23:56 +0900

    feat:#53 각 항목수량 합계 완료

--- src/components/cartPage/CartProduct.tsx
+++ src/components/cartPage/CartProduct.tsx
@@ -15,16 +15,17 @@ type CartProductBoxProps = {
 type listProps = {
     list: CartProductBoxProps[];
     onDeleteCart: (cartId: number) => void;
+    changeTotal: (price: number, count: string) => void
 }
 
-const CartProduct = ({ list, onDeleteCart }: listProps) => {
+const CartProduct = ({ list, onDeleteCart, changeTotal }: listProps) => {
 
     return (
         <div className="cart_product_container">
             <CartProductTitle/>
             {list?.map((item: CartProductBoxProps, idx: number) => {
                 return (
-                    <CartProductBox key={idx} item={item} onDeleteCart={onDeleteCart} />
+                    <CartProductBox key={idx} item={item} onDeleteCart={onDeleteCart} changeTotal={changeTotal}/>
                 )
             })}
         </div>
--- src/components/cartPage/CartTotalPrice.tsx
+++ src/components/cartPage/CartTotalPrice.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import CartBuyBtn from './CartBuyBtn';
 
-const CartTotalPrice = () => {
-  return (
-      <div className="cart_totalprice_container">
-          <div className="totalprice_title_wrapper">
-              <span>총 가격</span>
-          </div>
-          <div className="totalprice_wrapper">
-              <span>15,000원</span>
-          </div>
-          <CartBuyBtn/>
-      </div>
-  )
+type CartTotalProps = {
+    totalPrice: number | undefined
+}
+
+const CartTotalPrice = ({ totalPrice }: CartTotalProps) => {
+
+    const localePrice = totalPrice?.toLocaleString();
+
+    return (
+        <div className="cart_totalprice_container">
+            <div className="totalprice_title_wrapper">
+                <span>총 가격</span>
+            </div>
+            <div className="totalprice_wrapper">
+                <span>{localePrice}원</span>
+            </div>
+            <CartBuyBtn />
+        </div>
+    )
 }
 
 export default CartTotalPrice;
